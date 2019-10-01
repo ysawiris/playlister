28b966bf53f57c8ca35424d0f7ebc117af8a38bb
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-10-01 10:39:27 -0700

    Users can create playlists

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 
 app = Flask(__name__)
-
-# OUR MOCK ARRAY OF PROJECTS
-#playlists = [
-#    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-#   { 'title': '80\'s Music', 'description': 'Don\'t stop believing!'}
-#]
+'''
+OUR MOCK ARRAY OF PROJECTS
+playlists = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+   { 'title': '80\'s Music', 'description': 'Don\'t stop believing!'}
+]
+'''
 client = MongoClient()
 db = client.Playlister
 playlists = db.playlists
@@ -18,6 +19,22 @@ def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists.find())
 
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    """Submit a new playlist."""
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description')
+    }
+    
+    playlists.insert_one(playlist)
+    return redirect(url_for('playlists_index'))
 
+
+@app.route('/playlists/new')
+def playlists_new():
+    """Create a new playlist."""
+    return render_template('playlists_new.html')
+    
 if __name__ == '__main__':
     app.run(debug=True)
